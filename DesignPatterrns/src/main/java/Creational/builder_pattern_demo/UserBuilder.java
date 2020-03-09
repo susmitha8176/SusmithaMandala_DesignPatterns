f5100b4de67396264a4fc2f5968428a28bb40701
susmitha8176 <17wh1a0591@bvrithyderabad.edu.in>
2020-03-09 22:52:02 +0530

    first commit

---
+++ DesignPatterrns/src/main/java/Creational/builder_pattern_demo/UserBuilder.java
@@ -0,0 +1,28 @@
+package Creational.builder_pattern_demo;
+
+public class UserBuilder {
+	public static void main(String[] args) {
+	    UserDetails user1 = new UserDetails.UserBuilder("Lokesh")
+	    .age(30)
+	    .phone("1234567")
+	    .address("Fake address 1234")
+	    .build();
+	 
+	    System.out.println(user1);
+	 
+	    UserDetails user2 = new UserDetails.UserBuilder("Jack")
+	    .age(40)
+	    .phone("5655")
+	    .build();
+	 
+	    System.out.println(user2);
+	 
+	    UserDetails user3 = new UserDetails.UserBuilder("Johnson")
+	    
+	    .build();
+	 
+	    System.out.println(user3);
+	}
+	 
+
+}
---
+++ DesignPatterrns/src/main/java/Creational/factory_method_pattern/Commercial.java
@@ -0,0 +1,9 @@
+package Creational.factory_method_pattern;
+
+public class Commercial extends Plan {
+	@Override   
+    public void getRate(){   
+        rate=7.50;  
+   }   
+
+}
